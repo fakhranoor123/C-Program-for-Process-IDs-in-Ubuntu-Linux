30cb37474cbaf77e41cc5dd03e685dfff274e1ca
fakhranoor123 <93247792+fakhranoor123@users.noreply.github.com>
2021-11-12 20:09:49 +0500

    Add files via upload

--- Q2.c
+++ Q2.c
@@ -7,14 +7,14 @@ int main(void){
   int pid, status;
   pid = fork();
   if(pid==0){
-  int num = 8; // Child Process
+  int num = 2; // Child Process
    for(int i=1; i<=10; i++){
      printf("%d x %d = %d\n",num, i, num*i);
   }
  }
  else{ // Parent Process
    wait(&status);
-   printf("In the parent Process :/nName is Fakhra Noor and CNIC is 30000-1234567-8\n");
+   printf("In the parent Process :/nName is Fakhra Noor and CNIC is 30000-1234567-2\n");
  }
 
  return 0;
